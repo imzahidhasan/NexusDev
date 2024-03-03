148a17330470a3aa020fbac3423b2c87a81a9e66
Zahid Hasan <imzahidhasan.mail@gmail.com>
2024-03-03 17:06:59 +0600

    completed all post api intigraton and all dynamic function

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ async function getAllPosts() {
                     <div class=" border-b-2 border-dashed p-4 border-gray-400 mx-auto"></div>
                     <div class="flex justify-between gap-4 items-center md:p-2">
                         <div>
-                            <div class="flex gap-7  ">
+                            <div class="flex gap-7 ">
                                 <i class="fa-regular fa-message">   ${
                                   post?.comment_count
                                 }</i>
@@ -52,14 +52,42 @@ async function getAllPosts() {
                                 } min</i>
                             </div>
                         </div>
-                        <div class="text-3xl text-green-600">
-                            <button class="btn btn-ghost rounded-full"><i class="fa-solid fa-envelope-open-text text-green-500 text-xl"></i></button>
+                        <div class="text-3xl text-green-600 mb-2">
+                            <button onclick='btnClicked("${(post.title).replace("'",'')}", "${post.view_count}")' class="btn btn-ghost rounded-full"><i class="fa-solid fa-envelope-open-text text-green-500 text-xl"></i></button>
                         </div>
                     </div>
                 </div>
             </div>
         `;
+
     getById("all-posts-section").appendChild(div);
   });
 }
 getAllPosts();
+let count=0
+function btnClicked(title, viewCount) {
+  count++
+  getById("counter").innerText=count
+  const sideList = document.createElement("div");
+  sideList.innerHTML = `<div class="flex justify-between items-center bg-white rounded-xl mx-4 p-2 mb-2">
+                            <h1 class="w-[70%] p-2">${title}</h1>
+                            <h1 class="w-30%"><i class="fa-regular text-sm fa-eye"></i> ${viewCount}</h1>
+                        </div>`;
+  getById("sideBar").appendChild(sideList);
+ 
+}
+
+const getLatestPost = async () => {
+  const URL = `https://openapi.programming-hero.com/api/retro-forum/latest-posts`;
+  const res = await fetch(URL)
+  const data = await res.json()
+  data.forEach(post => {
+    const div = document.createElement('div')
+    div.innerHTML = `
+    
+    
+    `
+  })
+
+}
+getLatestPost()
